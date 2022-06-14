d442d57dc69ce02453fd5c7e62be1227051073cc
Owner <deema@danon.tzafonet.org.il>
2022-06-14 13:06:03 +0300

    TODO sign in and up and activity for the uploaded images

--- app/src/test/java/MyData/Adapter.java
+++ app/src/test/java/MyData/Adapter.java
@@ -1,4 +1,24 @@
 package MyData;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import com.amitshekhar.tflite.R;
+
 public class Adapter {
+
+    public void getView(int position, @NonNull View convertView, ViewGroup parent) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.withoutmask_item_layout, parent, false);
+        WithoutMask item = getItem(position);
+        ImageView img= v.findViewById(R.id.itmImg);
+        ImageView delete = v.findViewById(R.id.itmButtonDelete);
+    }
+
+    private WithoutMask getItem(int position) {
+    }
 }
+
